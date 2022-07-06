4ae80bc7057d7b156addfe69fffbeaf2dbe59f89
Jan Svoboda <janis.svobi@gmail.com>
2022-07-06 14:48:26 +0200

    test

--- duplicate_files.ps1
+++ duplicate_files.ps1
@@ -14,11 +14,8 @@ $hashes = @{}
 # Log file is in CSV format
 Out-File -FilePath $LogFile -InputObject "Puvodni umisteni,Nove umisteni" -Encoding utf8
 
-$color = $host.ui.RawUI.ForegroundColor
-$host.ui.RawUI.ForegroundColor = "DarkGreen"
-Write-Output "Duplicate files"
-Write-Output "---------------"
-$host.ui.RawUI.ForegroundColor = $color
+Write-Host "Duplicate files" -ForegroundColor DarkGreen
+Write-Host "---------------" -ForegroundColor DarkGreen
 
 $files = @(Get-ChildItem -Path $SrcDir -Recurse)
 foreach($file in $files) {
