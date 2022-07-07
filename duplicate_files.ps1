93323da8179009a1f745638fb49b7bcadfa66b63
predseda <janis.svobi@gmail.com>
2022-07-07 18:59:00 +0000

    Implement script + tests + Github Actions (#1)
    * Implement duplicate_files.ps1
    
    * Add tests
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * Test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * Fix
    
    * Implement tests
    
    * Fix path in tests
    
    * Modify tests workflow
    
    * Modify tests workflow
    
    * Modify tests workflow

---
+++ duplicate_files.ps1
@@ -0,0 +1,38 @@
+param(
+    [Parameter(Mandatory)]
+    [String]$SrcDir,
+
+    [Parameter(Mandatory)]
+    [String]$DstDir,
+
+    [Parameter(Mandatory)]
+    [String]$LogFile
+)
+
+$hashes = @{}
+
+# Log file is in CSV format
+Out-File -FilePath $LogFile -InputObject "Puvodni umisteni,Nove umisteni" -Encoding utf8
+
+Write-Host "Duplicate files" -ForegroundColor Green
+Write-Host "---------------" -ForegroundColor Green
+
+$files = @(Get-ChildItem -Path $SrcDir -Recurse)
+foreach($file in $files) {
+    $file_full_name = $file.FullName
+    $file_short_name = $file.Name
+
+    $is_file = (Test-Path -Path $file_full_name -PathType Leaf)  # Skip directories
+    if($is_file) {
+        $hash = (Get-FileHash $file_full_name)
+        If(-not($hash.Hash -in $hashes.keys)) {
+            $hashes[$hash.Hash] = $file_full_name
+        }
+        else {
+            Write-Output $file_full_name
+            Move-Item -Path $file_full_name -Destination $DstDir -Force
+            $new_dst = $DstDir + $file_short_name
+            Out-File -FilePath $LogFile -InputObject "$file_full_name,$new_dst" -Encoding utf8 -Append
+        }
+    }
+}
